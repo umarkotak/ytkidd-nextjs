661d9f701d16363c8163a748250ffe6382c96844
umar <umarkotak@gmail.com>
2023-08-19 20:18:13 +0700

    prepare statistic page

--- src/app/stats/page.js
+++ src/app/stats/page.js
@@ -4,7 +4,9 @@ import { useState, useEffect } from 'react'
 import { ComposedChart, Bar, LabelList, Cell, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 
 export default function Stats() {
-  const [activeIndex, setActiveIndex] = useState(0)
+  const [activeDate, setActiveDate] = useState({
+    name: "N/A"
+  })
   const [limitDays, setLimitDays] = useState(7)
   const [dateStat, setDateStat] = useState([])
 
@@ -42,19 +44,27 @@ export default function Stats() {
     })
 
     setDateStat(tempDateStat)
+    setActiveDate(tempDateStat[tempDateStat.length-1])
   }, [])
 
   const handleClick = (data, index) => {
-    setActiveIndex(index)
-  };
+    setActiveDate(data)
+  }
 
-  const activeItem = dateStat[activeIndex];
+  function AddSample() {
+    var key = `YTKIDD:DAILY_VIDEO_STAT:2023-8-17`
+    localStorage.setItem(key, JSON.stringify({
+      "total_watch_duration": 1000,
+      "view_count": 10,
+      "latest_watched_at_unix": 0
+    }))
+  }
 
   return (
     <main className="pb-[100px] p-4 w-full">
       <div className='mb-6'>
         <h1 className='text-xl'>Watch Statistic</h1>
-        <small>track how much you watch the video</small>
+        <small>track how much you watch video</small>
       </div>
       <div className="w-full h-[300px]">
         <ResponsiveContainer>
@@ -64,24 +74,34 @@ export default function Stats() {
             {/* <YAxis fill='#020617' /> */}
             <Tooltip/>
             <Legend/>
-            <Bar dataKey="total_watch_duration" onClick={handleClick} barSize={20} fill='#0284c7'>
+            <Bar dataKey="view_count" onClick={handleClick} barSize={20} fill='#0284c7'>
               {dateStat.map((entry, index) => (
                 <>
-                  <Cell cursor="pointer" fill={index === activeIndex ? '#0284c7' : '#0284c7'} key={`cell-${index}`}/>
+                  {console.log(entry)}
+                  <Cell cursor="pointer" fill={entry.name === activeDate.name ? '#e11d48' : '#0284c7'} key={`cell-${index}`}/>
                 </>
               ))}
             </Bar>
-            <Line type="monotone" dataKey="view_count" stroke="#16a34a">
+            <Line type="monotone" dataKey="total_watch_duration" stroke="#16a34a">
               <>
                 {/* <LabelList dataKey="view_count" fill={'#000000'} position="top"/> */}
               </>
             </Line>
           </ComposedChart>
         </ResponsiveContainer>
+
+        {/* <button onClick={()=>AddSample()} className='bg-gray-200 p-2 rounded-xl'>Add sample</button> */}
       </div>
 
       <div>
-        {/* <p className="content">{`Uv of "${activeItem.name}": ${activeItem.uv}`}</p> */}
+        <div className='mb-6'>
+          <div className='flex items-center'>
+            <button className='p-2 bg-gray-300 hover:bg-gray-400 rounded-lg mr-2'><i className='fa-solid fa-arrow-left'/></button>
+            <h1 className='text-xl mr-2'>{activeDate.name} Stat</h1>
+            <button className='p-2 bg-gray-300 hover:bg-gray-400 rounded-lg mr-2'><i className='fa-solid fa-arrow-right'/></button>
+          </div>
+          <small>track how much you watch video</small>
+        </div>
       </div>
     </main>
   )
